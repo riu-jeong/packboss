cab3e018bde2e04054bf487e779da66d7357e42a
riu-jeong <nodo1017@riupack.com>
2022-12-13 09:43:46 +0900

    퍼블

---
+++ tailwind.config.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  content: ["./app/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#D3E4F6",
+          200: "#ABC8ED",
+          300: "#7598CA",
+          400: "#476496",
+          500: "#182B50",
+          600: "#112144",
+          700: "#0C1839",
+          800: "#07102E",
+          900: "#040B26",
+        },
+        secondary: {
+          100: "#FFF0CC",
+          200: "#FFDC99",
+          300: "#FFC366",
+          400: "#FFAB3F",
+          500: "#FF8300",
+          600: "#DB6500",
+          700: "#B74C00",
+          800: "#933600",
+          900: "#7A2700",
+        },
+      },
+      fontFamily: {
+        sans: ['"Open Sans"'],
+      },
+      width: {
+        100: "25rem",
+      },
+      screens: {
+        xl: "1200px",
+      },
+      gridTemplateColumns: {
+        productInfo: "110px 1fr",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+      strategy: "class", // only generate classes
+    }),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
